25640a66d563d710d146841e043eb26ca2459c91
정삼복 <68502584+3boku@users.noreply.github.com>
2024-08-26 20:35:37 +0900

    init

--- controllers/root.go
+++ controllers/root.go
@@ -1,1 +1,30 @@
 package controllers
+
+import (
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+	"time"
+)
+
+func NewContorllers(port string) error {
+	// 라우터 생성
+	r := gin.Default()
+
+	r.Use(cors.New(cors.Config{
+		AllowOrigins: []string{"*"},
+		AllowMethods: []string{"GET", "POST", "PUT", "PATCH", "DELETE"},
+		MaxAge:       24 * time.Hour,
+	}))
+
+	v1 := r.Group("/api/v1")
+	{
+		v1.GET("", func(c *gin.Context) {
+			c.JSON(200, gin.H{
+				"message": "서버 이니셜라이징 성공!",
+			})
+		})
+	}
+
+	err := r.Run(port)
+	return err
+}
--- init/cmd/cmd.go
+++ init/cmd/cmd.go
@@ -1,1 +1,33 @@
 package cmd
+
+import (
+	"Back/controllers"
+	"github.com/naoina/toml"
+	"os"
+)
+
+type Config struct {
+	Server struct {
+		Port string
+	}
+	MySQL struct {
+		Host string
+	}
+	Mongo struct {
+		URI string
+	}
+}
+
+func NewCmd(filePath string) {
+	c := new(Config)
+	if file, err := os.Open(filePath); err != nil {
+		panic(err)
+	} else if err := toml.NewDecoder(file).Decode(c); err != nil {
+		panic(err)
+	}
+
+	err := controllers.NewContorllers(c.Server.Port)
+	if err != nil {
+		panic(err)
+	}
+}
